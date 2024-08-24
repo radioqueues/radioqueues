255426975567e8725d80f9218fdb6d8031a4c05a
Nils <nils.larp@outlook.com>
2024-08-25 00:23:45 +0200

    use material toolbar and checkbox

--- frontend/queuemanager/src/app/component/queue-window/queue-window.component.ts
+++ frontend/queuemanager/src/app/component/queue-window/queue-window.component.ts
@@ -1,19 +1,24 @@
 import { Component, Input } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { Queue } from 'src/app/model/queue';
 import { QueueComponent } from '../queue/queue.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-queue-window',
 	templateUrl: './queue-window.component.html',
 	styleUrl: './queue-window.component.css',
 	standalone: true,
-	imports: [FormsModule, MatExpansionModule, QueueComponent],
+	imports: [FormsModule, MatCheckboxModule, MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule, QueueComponent],
 })
 export class QueueWindowComponent {
-	@Input({required: true}) queue!: Queue;
+	@Input({ required: true }) queue!: Queue;
 	@Input() readonly?: boolean;
 	@Input() subQueueContentToggle: boolean = false;
 	showSubQueueContent: boolean = true;

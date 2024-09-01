f4a5df5b32b36e1a30f15f0810b06d93cab0610d
Nils <nils.larp@outlook.com>
2024-09-01 12:06:15 +0200

    use flex layout

--- frontend/queuemanager/src/app/component/queuemanager/queue-manager-main-window.component.ts
+++ frontend/queuemanager/src/app/component/queuemanager/queue-manager-main-window.component.ts
@@ -27,18 +27,18 @@ export class QueueManagerMainWindowComponent {
 	queues = MockData.queues;
 
 	constructor() {
-/*		this.queues[1][0].entries.push(this.queues[2][0]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 10:03:20", duration: (11*60+40)*1000, color: "#AAA" });
-*/		this.queues[1][0].entries.push(this.queues[3][0]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 10:16:22", duration: (28 * 60 + 38) * 1000, color: "#AAA" });
-		this.queues[1][0].entries.push(this.queues[3][1]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 10:46:34", duration: (13 * 60 + 26) * 1000, color: "#AAA" });
-		this.queues[1][0].entries.push(this.queues[2][1]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 11:04:08", duration: (10 * 60 + 52) * 1000, color: "#AAA" });
-		this.queues[1][0].entries.push(this.queues[3][2]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 11:17:03", duration: (12 * 60 + 57) * 1000, color: "#AAA" });
-		this.queues[1][0].entries.push(this.queues[2][2]);
-		this.queues[1][0].entries.push({ "name": "Musik", "offset": "2024-08-16 11:31:40", duration: (13 * 60 + 20) * 1000, color: "#AAA" });
+/*		this.queues[1].entries.push(this.queues[2]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 10:03:20", duration: (11*60+40)*1000, color: "#AAA" });
+*/		this.queues[1].entries.push(this.queues[7]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 10:16:22", duration: (28 * 60 + 38) * 1000, color: "#AAA" });
+		this.queues[1].entries.push(this.queues[8]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 10:46:34", duration: (13 * 60 + 26) * 1000, color: "#AAA" });
+		this.queues[1].entries.push(this.queues[3]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 11:04:08", duration: (10 * 60 + 52) * 1000, color: "#AAA" });
+		this.queues[1].entries.push(this.queues[9]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 11:17:03", duration: (12 * 60 + 57) * 1000, color: "#AAA" });
+		this.queues[1].entries.push(this.queues[4]);
+		this.queues[1].entries.push({ "name": "Musik", "offset": "2024-08-16 11:31:40", duration: (13 * 60 + 20) * 1000, color: "#AAA" });
 	}
 
 	onSettingsClicked() {

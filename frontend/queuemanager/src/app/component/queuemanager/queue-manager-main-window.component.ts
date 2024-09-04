5161c7e1ad71fa8f6ab84d46d4ec0718cf6e816b
Nils <nils.larp@outlook.com>
2024-09-04 21:34:04 +0200

    some migration logic to create queues from a history file

--- frontend/queuemanager/src/app/component/queuemanager/queue-manager-main-window.component.ts
+++ frontend/queuemanager/src/app/component/queuemanager/queue-manager-main-window.component.ts
@@ -64,4 +64,72 @@ export class QueueManagerMainWindowComponent implements OnInit {
 	onPickRoot() {
 		this.fileSystemService.pickRoot();
 	}
+
+	onDebugClicked() {
+		let history = this.queues["m0lhjdqh-a78095b4-0c65-44a7-9555-bdcbd93d00a6"];
+		let mainQueue = this.queues["m0lhjegw-8deb427b-ec8e-40de-a4ba-b30dbbcd5e0b"];
+		console.log(history);
+		let lastQueueType = "";
+		let queue: Queue|undefined = undefined;
+		for (let entry of history.entries) {
+
+			let queueTypeName = "Musik";
+			if (entry.name!.indexOf("Nachrichten") > -1) {
+				queueTypeName = "Nachrichten";
+			} else if (entry.name!.indexOf("Worlds Best Hits") > -1) {
+				queueTypeName = "Nachrichten";
+			} else if (entry.name!.indexOf("Werbung") > -1) {
+				queueTypeName = "Werbung";
+			} else if (entry.name!.indexOf("Vorbereitet/FPS_Intro") > -1) {
+				queueTypeName = "Werbung";
+			}
+			if (lastQueueType != "Nachrichten" && queueTypeName == "Nachrichten" && entry.name!.indexOf("Jingle") < 0) {
+				queueTypeName = "Reportage";
+			}
+			
+
+			if (queueTypeName != lastQueueType) {
+				let queueType = this.queueTypes[queueTypeName];
+				if (!queueType) {
+					queueType = this.queueTypes["Musik"];
+				}
+				if (queue) {
+					mainQueue.entries.push({
+						"queueRef": queue.uuid,
+						"name": queue.name,
+						"offset": queue.offset,
+						"duration": queue.duration,
+						"color": queue.color
+					});
+				}
+				queue = queue = {
+					uuid: "zzzz" + entry.offset + "-" + crypto.randomUUID(),
+					name: queueTypeName + " " + (entry.offset as string).substring(11, 16),
+					offset: entry.offset,
+					color: queueType.color,
+					duration: 0,
+					type: queueType.name,
+					entries: []
+				};
+				this.queues[queue.uuid] = queue;
+				lastQueueType = queueTypeName;
+			}
+			queue?.entries.push(entry);
+			if (entry?.duration && entry?.duration > 0) {
+				queue!.duration = queue!.duration! + entry.duration;
+			}
+		}
+		if (queue) {
+			mainQueue.entries.push({
+				"queueRef": queue.uuid,
+				"name": queue.name,
+				"offset": queue.offset,
+				"duration": queue.duration,
+				"color": queue.color
+			});
+		}
+
+				
+		console.log(this.queues);
+	}
 }

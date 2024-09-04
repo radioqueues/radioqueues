37df1f99819a253c5f22ea1d4d5f82beee120064
Nils <nils.larp@outlook.com>
2024-09-04 20:30:05 +0200

    duration in seconds

--- frontend/queuemanager/src/app/component/queue/queue.component.ts
+++ frontend/queuemanager/src/app/component/queue/queue.component.ts
@@ -158,11 +158,11 @@ export class QueueComponent {
 	}
 
 	formatDuration(duration?: number) {
-		if (!duration) {
+		if (!duration || duration < 0) {
 			return "??:??";
 		}
-		let minutes = Math.floor(duration / 1000 / 60);
-		let seconds = Math.floor(duration / 1000 % 60);
+		let minutes = Math.floor(duration / 60);
+		let seconds = Math.floor(duration % 60);
 		return this.pad2(minutes) + ":" + this.pad2(seconds);
 	}
 

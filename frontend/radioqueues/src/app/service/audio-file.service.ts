5c0ede45c9187330252ec619203b9143717d34ad
Nils <nils.larp@outlook.com>
2024-09-12 22:56:41 +0200

    ensure that loadDurations only returns after all work is done

--- frontend/radioqueues/src/app/service/audio-file.service.ts
+++ frontend/radioqueues/src/app/service/audio-file.service.ts
@@ -25,39 +25,55 @@ export class AudioFileService {
 		return known.size === metadata.size && known.lastModified === metadata.lastModified;
 	}
 
-	private async loadDurations(loadedFiles: any) {
-		let files = await this.databaseService.getFiles();
+	private updateStatus(current: number, count: number) {
 		this.process.next({
-			count: Object.keys(loadedFiles).length,
-			current: 0
+			current: current,
+			count: count
 		});
+		console.log("status", current, count);
+	}
+
+	async loadDurations(loadedFiles: any) {
+		let files = await this.databaseService.getFiles();
 		let i = 0;
+		let count = Object.keys(loadedFiles).length;
+		this.updateStatus(i, count);
+
+		// do not use an async for loop because it would do all disk reads first and then all calculations
 		for (let filename in loadedFiles) {
-			i++;
 			if (this.isFileKnown(files, filename, loadedFiles[filename])) {
-				continue;
+				i++;
+				this.updateStatus(i, Object.keys(loadedFiles).length);
 			}
-			let fileHandle = await this.fileSystemService.getFileHandle(filename);
-			let blob = await fileHandle?.getFile();
-			if (blob) {
-				let audio = new Audio();
-				audio.src = URL.createObjectURL(blob!)
-				audio.addEventListener("loadedmetadata", () => {
-					loadedFiles[filename].duration = audio.duration * 1000;
-				})
-				audio.addEventListener("error", (event) => {
+			let promise = new Promise<void>(async (resolve) => {
+				let fileHandle = await this.fileSystemService.getFileHandle(filename);
+				let blob = await fileHandle?.getFile();
+				if (blob) {
+					let audio = new Audio();
+					audio.src = URL.createObjectURL(blob!)
+					audio.addEventListener("loadedmetadata", () => {
+						loadedFiles[filename].duration = audio.duration * 1000;
+						i++;
+						this.updateStatus(i, count);
+						resolve();
+					})
+					audio.addEventListener("error", (event) => {
+						loadedFiles[filename].duration = undefined;
+						console.log("error", filename, event);
+						i++;
+						this.updateStatus(i, count);
+						resolve();
+					});
+				} else {
+					console.log("File not found", filename);
 					loadedFiles[filename].duration = undefined;
-					console.log("error", filename, event);
-				});
-			} else {
-				console.log("File not found", filename);
-				loadedFiles[filename].duration = undefined;
-			}
-			files[filename] = loadedFiles[filename];
-			this.process.next({
-				count: Object.keys(loadedFiles).length,
-				current: i
+					i++;
+					this.updateStatus(i, count);
+					resolve();
+				}
+				files[filename] = loadedFiles[filename];
 			});
+			await promise;
 		}
 		this.databaseService.saveFiles();
 		this.process.next(undefined);

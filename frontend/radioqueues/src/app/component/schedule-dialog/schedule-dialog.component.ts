ba95a462e88d3d77ca4d84100e97370853989351
Nils <nils.larp@outlook.com>
2024-09-22 13:28:04 +0200

    implemented Schedule dialog

--- frontend/radioqueues/src/app/component/schedule-dialog/schedule-dialog.component.ts
+++ frontend/radioqueues/src/app/component/schedule-dialog/schedule-dialog.component.ts
@@ -2,22 +2,57 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
+  MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 
+import {MatRadioModule} from '@angular/material/radio';
+import { ErrorService } from 'src/app/service/error.service';
+import { DateTimeUtil } from 'src/app/util/date-time-util';
 
 @Component({
 	selector: 'app-schedule-dialog',
 	templateUrl: './schedule-dialog.component.html',
 	styleUrl: './schedule-dialog.component.css',
 	standalone: true,
-	imports: [CommonModule, FormsModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+	imports: [
+		CommonModule, FormsModule,
+		MatButtonModule,
+		MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose,
+		MatRadioModule
+	]
 })
 export class ScheduleDialogComponent {
-	time: string = "xx:xx";
+
+	private dialogRef: MatDialogRef<ScheduleDialogComponent, "next"|Date> = inject(MatDialogRef);
+	private errorService = inject(ErrorService);
+
+	time: string = "";
+	option: string = "schedule";
+
+	onTimeClicked() {
+		console.log(this.option);
+		this.option = "schedule";
+	}
+
+	onOkClicked() {
+		if (this.option == "schedule") {
+			try {
+				let time = DateTimeUtil.parseTime(this.time);
+				let date = DateTimeUtil.datetimeFromTime(new Date(), time);
+				this.dialogRef.close(date);
+			} catch (error) {
+				if (error instanceof Error) {
+					this.errorService.errorDialog(error.message);
+				}
+			}
+			return;	
+		}
+		this.dialogRef.close("next");
+	}
 }

8f93f33497e1cc68c1a7c7692198f2696a547ec2
Nils <nils.larp@outlook.com>
2024-11-04 23:25:57 +0100

    removed debug messages

--- frontend/radioqueues/src/app/component/queue-window/queue-window.component.ts
+++ frontend/radioqueues/src/app/component/queue-window/queue-window.component.ts
@@ -73,10 +73,6 @@ export class QueueWindowComponent {
 	}
 
 	isOverlaping(rect1: Rect, rect2: Rect) {
-		console.log(rect1.left < rect2.left + rect2.width, 
-					rect1.left + rect1.width > rect2.left,
-					rect1.top < rect2.top + rect2.height,
-					rect1.top + rect1.height > rect2.top)
 		return rect1.left < rect2.left + rect2.width
 			&& rect1.left + rect1.width > rect2.left
 			&& rect1.top < rect2.top + rect2.height

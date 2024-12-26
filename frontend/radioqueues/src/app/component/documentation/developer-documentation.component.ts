dd8d637fffe36f2d8dbbce7c60a0e4cd1b04cd6b
Nils <nils.larp@outlook.com>
2024-12-26 20:50:44 +0100

    documented algorithm for picking the next audio file

---
+++ frontend/radioqueues/src/app/component/documentation/developer-documentation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-developer-documentation',
+	templateUrl: './developer-documentation.component.html',
+})
+export class DeveloperDocumentationComponent {
+
+}

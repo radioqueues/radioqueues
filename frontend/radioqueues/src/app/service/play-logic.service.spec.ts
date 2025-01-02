78d372a0e036c36f6b7d9a6d1af9b2e6704bbe8b
Nils <nils.larp@outlook.com>
2025-01-02 23:04:09 +0100

    tests for PlayService.logic()

--- frontend/radioqueues/src/app/service/play-logic.service.spec.ts
+++ frontend/radioqueues/src/app/service/play-logic.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PlayService } from './play.service';
-import { Entry } from '../model/entry';
 import { QueueService } from './queue.service';
 import { DatabaseTestUtil } from './database.testutil';
 import { DateTimeUtil } from '../util/date-time-util';
 import { Queue } from '../model/queue';
+import { SECONDS } from '../model/time';
+import { DynamicQueueService } from './dynamic-queue.service';
+import { DatabaseService } from './database.service';
+import { FileSystemService } from './filesystem.service';
 
 describe('PlayService (logic)', () => {
 	let queueService: QueueService;
@@ -13,24 +16,43 @@ describe('PlayService (logic)', () => {
 	let mainQueue: Queue;
 
 	beforeEach(() => {
+		let dynamicQueueServiceSpy = jasmine.createSpyObj('DynamicQueueService', ['scheduleQueue']);
+		let fileSystemServiceSpy = jasmine.createSpyObj('FileSystemService', ['init', 'saveJsonToFilename']);
 		TestBed.configureTestingModule({
-
+			providers: [
+				{provide: DynamicQueueService, useValue: dynamicQueueServiceSpy},
+				{provide: FileSystemService, useValue: fileSystemServiceSpy},
+			]
 		});
+
 		queueService = TestBed.inject(QueueService);
 		queueService.queues = structuredClone(DatabaseTestUtil.queues);
 		queueService.queueTypes = structuredClone(DatabaseTestUtil.queueTypes);
 		queueService.files = structuredClone(DatabaseTestUtil.files);
 
+		let databaseService = TestBed.inject(DatabaseService);
+		databaseService.initTest(queueService.queues, queueService.queueTypes, queueService.files);
+
 		playService = TestBed.inject(PlayService);
 		playService.queues = queueService.queues;
 
 		mainQueue = queueService.getQueueByType("Main Queue")!;
+
+		// make the subset-sum queue just a bit too short, in order to test recalculation of the following queues
+		dynamicQueueServiceSpy.scheduleQueue.and.callFake((queueTypeName: string, duration: number) => {
+			let queueType = DatabaseTestUtil.queueTypes[queueTypeName];
+			let shorterDuration = duration - 10 * SECONDS;
+			let filename = queueType.folder! + "/file_" + (shorterDuration / SECONDS) + ".mp3";
+			queueService.files[filename] = { duration: shorterDuration };
+			return [filename];
+		});
+
 	});
 
-	it('should pick start', () => {
+	it('should pick start', async () => {
 		DateTimeUtil.fakeNow = new Date("3025-01-01T00:00:01.383Z");
 		playService.current = [];
-		playService.logic();
+		await playService.logic();
 		let current = playService.current;
 		expect(current).toEqual([
 			{ "color": "#0A0", "name": "News 01:00", "offset": new Date("3025-01-01T00:00:00.383Z"), "duration": 27216, "queueRef": "m5cxtb0g-b3709016-1dce-4bc7-b279-7edb79049bb6", "scheduled": new Date("3025-01-01T00:00:00.383Z") },
@@ -38,10 +60,10 @@ describe('PlayService (logic)', () => {
 		]);
 	});
 
-	fit('should pick next in same queue', () => {
+	it('should pick next in same queue', async () => {
 		DateTimeUtil.fakeNow = new Date("3025-01-01T00:00:01.383Z");
 		playService.current = [mainQueue.entries[0], queueService.resolveQueue(mainQueue.entries[0]).entries[1]];
-		playService.logic();
+		await playService.logic();
 		let current = playService.current;
 		expect(current).toEqual([
 			{ "color": "#0A0", "name": "News 01:00", "offset": new Date("3025-01-01T00:00:00.383Z"), "duration": 27216, "queueRef": "m5cxtb0g-b3709016-1dce-4bc7-b279-7edb79049bb6", "scheduled": new Date("3025-01-01T00:00:00.383Z") },
@@ -49,6 +71,22 @@ describe('PlayService (logic)', () => {
 		]);
 	});
 
+	it('should fill subset-sum queue', async () => {
+		DateTimeUtil.fakeNow = new Date("3025-01-01T00:00:26.123Z");
+		playService.current = [mainQueue.entries[0], queueService.resolveQueue(mainQueue.entries[0]).entries[3]];
+		console.log(queueService.queues, queueService.files);
+
+		await playService.logic();
+		let current = playService.current;
+		console.log(queueService.queues, queueService.files, current);
+		expect(current.length).toBe(2);
+		// expect(current[0].offset?.toString()).toBe(new Date("3025-01-01T00:00:26.123Z").toString());
+		expect(current[0].offset?.toString()).toEqual(new Date("3025-01-01T00:00:27.123Z").toString());
+		
+		let next = playService.pickNextQueue(current);
+		// expect(next?.offset?.toString()).toEqual(new Date("3025-01-01T00:14:51.123Z").toString());
+		expect(next?.offset?.toString()).toEqual(new Date("3025-01-01T00:14:52.123Z").toString());
+	});
 
 });
 
